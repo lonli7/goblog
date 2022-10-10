2f8750d0772b31c64ad56a1b6cc07259dbde8c88
lon <evens@bytensor.net>
2022-10-10 15:11:54 +0800

    新增关于页面

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ func handlerFunc(w http.ResponseWriter, r *http.Request) {
 	} else if r.URL.Path == "/about" {
 		fmt.Fprint(w, "此博客是用以记录编程笔记，如您有反馈或建议，请联系")
 	} else {
-		fmt.Fprint(w, "<h1>请求页面未找到:(</h1>" + "<p>如有疑惑，请联系我们</p>")
+		fmt.Fprint(w, "<h1>请求页面未找到:(</h1>"+"<p>如有疑惑，请联系我们</p>")
 	}
 }
 
