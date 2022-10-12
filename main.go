9dbb2e00d4b7bdfaf0976b065e0426c8a31fc3f0
lon <evens@bytensor.net>
2022-10-12 16:19:51 +0800

    修改入口文件

--- main.go
+++ main.go
@@ -1,19 +1,16 @@
 package main
 
 import (
-	"github.com/gorilla/mux"
 	"github.com/lonli7/goblog/app/http/middlewares"
 	"github.com/lonli7/goblog/bootstrap"
+	"github.com/lonli7/goblog/pkg/logger"
 	"net/http"
 )
 
-var router *mux.Router
-
-
-
 func main() {
 	bootstrap.SetupDB()
-	router = bootstrap.SetupRoute()
+	router := bootstrap.SetupRoute()
 
-	http.ListenAndServe("localhost:8080", middlewares.RemoveTrailingSlash(router))
+	err := http.ListenAndServe("localhost:8080", middlewares.RemoveTrailingSlash(router))
+	logger.LogError(err)
 }
