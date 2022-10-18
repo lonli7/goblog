96e6cae5e2a94effd86bcbf466c02baf6a1f3244
lon <evens@bytensor.net>
2022-10-18 10:56:58 +0800

    美化文章表单页

--- app/http/controllers/articles.go
+++ app/http/controllers/articles.go
@@ -8,7 +8,6 @@ import (
 	"github.com/lonli7/goblog/pkg/route"
 	"github.com/lonli7/goblog/pkg/view"
 	"gorm.io/gorm"
-	"html/template"
 	"net/http"
 	"strconv"
 	"unicode/utf8"
@@ -16,7 +15,7 @@ import (
 
 type ArticlesFormData struct {
 	Title, Body string
-	URL         string
+	Article     article.Article
 	Errors      map[string]string
 }
 
@@ -43,7 +42,7 @@ func (a *ArticlesController) Show(w http.ResponseWriter, r *http.Request) {
 		}
 	} else {
 		// 读取成功，显示文章
-		view.Render(w, "articles.show", articles)
+		view.Render(w, articles, "articles.show")
 	}
 }
 
@@ -55,7 +54,7 @@ func (a *ArticlesController) Index(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusInternalServerError)
 		fmt.Fprint(w, "500 服务器内部错误")
 	} else {
-		view.Render(w, "articles.index", articles)
+		view.Render(w, articles, "articles.index")
 	}
 }
 
@@ -79,22 +78,7 @@ func validateArticleFormData(title string, body string) map[string]string {
 }
 
 func (a *ArticlesController) Create(w http.ResponseWriter, r *http.Request) {
-	storeURL := route.Name2URL("articles.store")
-	data := ArticlesFormData{
-		Title:  "",
-		Body:   "",
-		URL:    storeURL,
-		Errors: nil,
-	}
-	tmpl, err := template.ParseFiles("resources/views/articles/create.gohtml")
-	if err != nil {
-		panic(err)
-	}
-
-	err = tmpl.Execute(w, data)
-	if err != nil {
-		panic(err)
-	}
+	view.Render(w, ArticlesFormData{}, "articles.create", "articles._form_field")
 }
 
 func (a *ArticlesController) Store(w http.ResponseWriter, r *http.Request) {
@@ -121,18 +105,11 @@ func (a *ArticlesController) Store(w http.ResponseWriter, r *http.Request) {
 			fmt.Fprint(w, "创建文章失败，请联系管理员")
 		}
 	} else {
-		storeURL := route.Name2URL("articles.store")
-		data := ArticlesFormData{
+		view.Render(w, ArticlesFormData{
 			Title:  title,
 			Body:   body,
-			URL:    storeURL,
 			Errors: errors,
-		}
-		tmpl, err := template.ParseFiles("resources/views/articles/create.gohtml")
-		logger.LogError(err)
-
-		err = tmpl.Execute(w, data)
-		logger.LogError(err)
+		}, "articles.create", "articles._form_field")
 	}
 }
 
@@ -151,18 +128,12 @@ func (a *ArticlesController) Edit(w http.ResponseWriter, r *http.Request) {
 			fmt.Fprint(w, "500 服务器错误")
 		}
 	} else {
-		updateURL := route.Name2URL("articles.update", "id", id)
-		data := ArticlesFormData{
-			Title:  _article.Title,
-			Body:   _article.Body,
-			URL:    updateURL,
-			Errors: nil,
-		}
-		tmpl, err := template.ParseFiles("resources/views/articles/edit.gohtml")
-		logger.LogError(err)
-
-		err = tmpl.Execute(w, data)
-		logger.LogError(err)
+		view.Render(w, ArticlesFormData{
+			Title:   _article.Title,
+			Body:    _article.Body,
+			Article: _article,
+			Errors:  nil,
+		}, "articles.edit", "articles._form_field")
 	}
 }
 
@@ -209,19 +180,12 @@ func (a *ArticlesController) Update(w http.ResponseWriter, r *http.Request) {
 			}
 
 		} else {
-			// 表单验证不通过，显示理由
-			updateURL := route.Name2URL("articles.update", "id", id)
-			data := ArticlesFormData{
-				Title:  title,
-				Body:   body,
-				URL:    updateURL,
-				Errors: errors,
-			}
-			tmpl, err := template.ParseFiles("resources/views/articles/edit.gohtml")
-			logger.LogError(err)
-
-			err = tmpl.Execute(w, data)
-			logger.LogError(err)
+			view.Render(w, ArticlesFormData{
+				Title: _article.Title,
+				Body: _article.Body,
+				Article: _article,
+				Errors: nil,
+			}, "articles.edit", "articles._form_field")
 		}
 	}
 }
--- pkg/view/view.go
+++ pkg/view/view.go
@@ -10,22 +10,24 @@ import (
 )
 
 // Render 渲染视图
-func Render(w io.Writer, name string, data interface{}) {
+func Render(w io.Writer, data interface{}, tplFiles ...string) {
 	// 设置模板相对路径
 	viewDir := "resources/views/"
 
-	// 将 articles.show 更正为 articles/show
-	name = strings.Replace(name, ".", "/", -1)
+	for i, f := range tplFiles {
+		tplFiles[i] = viewDir + strings.Replace(f, ".", "/", -1) + ".gohtml"
+	}
 
-	files, err := filepath.Glob(viewDir + "layouts/*.gohtml")
+	// 将 articles.show 更正为 articles/show
+	layoutFiles, err := filepath.Glob(viewDir + "layouts/*.gohtml")
 	logger.LogError(err)
 
-	newFiles := append(files, viewDir+name+".gohtml")
+	allFiles := append(layoutFiles, tplFiles...)
 
-	tmpl, err := template.New(name + ".gohtml").
+	tmpl, err := template.New("").
 		Funcs(template.FuncMap{
 			"RouteName2URL": route.Name2URL,
-		}).ParseFiles(newFiles...)
+		}).ParseFiles(allFiles...)
 	logger.LogError(err)
 
 	//渲染模板
